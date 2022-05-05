48d7ac5399df9037caed60adc82aef7dc1e0db9d
bvenceslas <bvenceslas@gmail.com>
2022-05-05 10:22:46 +0200

    initial code commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
 import { robots } from './components/robots';
 import CardList from './components/CardList';
+import SearchBox from "./components/SearchBox";
+
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      robots: robots
+    }
+  }
+
+  onSearchChange = (event) => {
+    console.log(event)
+  }
+
   render() {
-    return (
-      <CardList robots={robots}/>
-    );
+      return (
+        <div className="tc">
+          <h2>RoboFriends</h2>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <CardList robots={this.state.robots}/>
+        </div>
+      );
   }
 }
 
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,15 @@
+
+const SearchBox = ({searchField, searchChange}) => {
+    return (
+        <div className="pa2">
+            <input
+                className="pa3 ba b--green bg-lightest-blue"
+                type="search" 
+                placeholder="Search"
+                onChange={searchChange}
+            />
+        </div>
+    )
+}
+
+export default SearchBox;
